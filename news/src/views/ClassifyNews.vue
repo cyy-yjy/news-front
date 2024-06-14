1199afb99ed76435b010400f00526d5d994ea7d0
a-little-dust <1429692815@qq.com>
2024-06-15 01:36:10 +0800

    新闻分类 完成与后端联调 前端待优化（比如没做历史记录）

--- news/src/views/ClassifyNews.vue
+++ news/src/views/ClassifyNews.vue
@@ -25,16 +25,35 @@
         <div class="history-tags" >
         <span>分类结果：</span>
         <el-tag
-          v-for="(history, index) in reversedhistoryRecords"
+          v-for="(data, index) in classifyResult"
           :key="index"
           class="tags"
-          :class="{ 'tag-selected': selectedTagIndex === index }"
+          :disable-transitions="false"
+        @close="handleClose(data)"
           closable
-          @click="openHistory(index)"
-          @close="deleteHistory(index)"
+          :type="data.style? data.style :'success'"
         >
-          {{ history.front }}
+          {{ data.type }},
+          <template v-if="data.probability !== undefined">
+        概率为{{ data.probability.toFixed(2) }}%
+      </template>
+      <template v-else>
+  是您自定义的
+          </template>
         </el-tag>
+        <!-- 以下是输入框，允许用户输入自己想添加的标签 -->
+        <el-input
+        v-if="inputVisible"
+        ref="InputRef"
+        v-model="inputValue"
+        class="w-20"
+        size="small"
+        @keyup.enter="handleInputConfirm()"
+        @blur="handleInputConfirm()"
+      />
+      <el-button v-else class="button-new-tag" size="small" @click="showInput()">
+        + New Tag
+      </el-button>
       </div>
       </div>
     </div>
@@ -42,15 +61,41 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { ElMessage } from "element-plus";
+import { ref, onMounted, nextTick } from "vue";
+import { ElMessage, ElInput } from "element-plus";
 import "element-plus/theme-chalk/index.css";
 import origin from "@/apis/origin";
 
 const titleInput = ref("");
-const classifyResult = ref("");
+// 得到一个数组
+const classifyResult = ref([]);
 const historyRecords = ref([]);
-const resultClasses=ref([]);
+const inputValue = ref('')
+const inputVisible = ref(false)
+const InputRef = ref(null);
+const handleClose = (data) => {//已完成 让标签能被删掉
+  classifyResult.value.splice(classifyResult.value.indexOf(data), 1)
+}
+
+const showInput = () => {//已完成 让输入框显示出光标
+  inputVisible.value = true
+  nextTick(() => {
+    if (InputRef.value && InputRef.value.input) {
+      InputRef.value.input.focus();
+    }
+  })
+}
+//如果用户输入完毕 需要把内容加入到tag列表
+const handleInputConfirm = () => {
+  if (inputValue.value) {
+    const tmp = {}
+    tmp.type = inputValue.value
+    tmp.style='warning'
+    classifyResult.value.push(tmp)
+  }
+  inputVisible.value = false
+  inputValue.value = ''
+}
 onMounted(() => {
   console.log("开始渲染");
   console.log(localStorage.getItem("title"))
